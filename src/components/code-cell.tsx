47933c403d025ca5a2f34d72fcd824e95c2c5454
Dani <dani.tasheva@yahoo.com>
2023-07-18 12:01:06 -0600

    cumulative code

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -15,43 +15,45 @@ interface CodeCellProps {
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
-  
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
   const cumulativeCode = useTypedSelector((state) => {
     const { data, order } = state.cells;
-    const orderedCells = order.map(id => data[id]);
+    const orderedCells = order.map((id) => data[id]);
 
     const cumulativeCode = [];
 
     for (let c of orderedCells) {
       if (c.type === "code") {
         cumulativeCode.push(c.content);
-      } 
+      }
       if (c.id === cell.id) {
-        break
+        break;
       }
     }
     return cumulativeCode;
-
   });
 
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      // createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join("\n"));
       return;
     }
 
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      // createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join("\n"));
     }, 750);
 
     return () => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.content, cell.id, createBundle]);
-console.log("cumulativeCode", cumulativeCode)
+  }, [cumulativeCode.join("\n"), cell.id, createBundle]);
+
+  console.log("cumulativeCode", cumulativeCode);
+
   return (
     <Resizable direction="vertical">
       <div
